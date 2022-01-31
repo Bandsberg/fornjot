4725ecc8e1a292b2d9ef734cda5382637b674918
Bandsberg <rodekassen@bandsberg.dk>
2022-01-31 17:46:09 +0100

    Mocking mockall - Movement experiements

--- src/input/movement.rs
+++ src/input/movement.rs
@@ -1,8 +1,36 @@
+use crate::{camera::Camera, math::Point};
+use cfg_if::cfg_if;
+#[cfg(test)]
+use mockall;
+use mockall_double;
 use nalgebra::distance;
 use winit::dpi::PhysicalPosition;
 
-use crate::{camera::Camera, math::Point, window::Window};
+//Setting up mock of Window / use window::Window
+//Måske bare mocke i window craten??? og importere den mock og bruge den.
+//Så tænker jeg namespace burde være correct.
+cfg_if! {
 
+    if #[cfg(test)] {
+        use mockall::*;
+        mock! {
+            pub Window{
+                fn height(&self) -> u32;
+                fn width(&self) -> u32;
+            }
+        }
+        use MockWindow as Window;
+
+    }
+
+    else {
+        use crate::window::Window;
+    }
+
+
+}
+
+#[derive(PartialEq)]
 pub struct Movement {
     focus_point: Option<Point<3>>,
     cursor: Option<PhysicalPosition<f64>>,
@@ -52,5 +80,85 @@ impl Movement {
         }
 
         self.cursor = cursor;
+    }
+}
+/*
+#[cfg(test)]
+mockall::mock! {pub Window{
+    fn height() -> u32;
+}}
+*/
+/*
+#[cfg(not(test))]
+use foo::Foo;
+#[cfg(test)]
+use foo::MockFoo as Foo;
+*/
+
+//#[double]
+//use Window::height;
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use crate::{camera::Camera, math::Point};
+    use mockall;
+    use mockall_double::double;
+    use nalgebra::Vector3;
+    use parry3d_f64::bounding_volume::AABB;
+    use winit::dpi::PhysicalPosition;
+
+    #[test]
+    fn test_new() {
+        let movement = Movement {
+            focus_point: None,
+            cursor: None,
+        };
+        assert!(Movement::new() == movement);
+    }
+
+    #[test]
+    fn test_stop() {
+        let mut movement = Movement {
+            focus_point: Some(Point::origin()),
+            cursor: None,
+        };
+        movement.stop();
+        assert_eq!(movement.focus_point, None);
+    }
+
+    //TODO
+    #[test]
+    fn test_apply() {
+        //cursor
+        let cursor: Option<PhysicalPosition<f64>> =
+            Some(PhysicalPosition::new(1.0, 1.0));
+
+        //camera: &mut Camera,
+        //Camera: pub fn new(aabb: &AABB) -> Self
+        //aabb: pub fn new(mins: Point<Real>, maxs: Point<Real>) -> AABB
+        //Point:
+        let min_point = Point::from(Vector3::new(1.0, 1.0, 1.0));
+        let max_point = Point::from(Vector3::new(100.0, 100.0, 100.0));
+        let aabb = AABB::new(min_point, max_point);
+        let mut camera = Camera::new(&aabb);
+
+        //Window. Mock window for consitency(reproducability) and simplcity
+        //Use mockall_double and mockall, hopefully it will work :S
+        /*
+                mockall::mock! {
+                    pub Window{
+                        fn height(&self) -> u32;
+                        fn width(&self) -> u32;
+                    }
+                }
+                use MockWindow as Window;
+        */
+        let mut mock = MockWindow::new();
+        mock.expect_height().return_const(100u32);
+
+        let movement = Movement::new();
+        movement.apply(cursor, &mut camera, &mock);
     }
 }
